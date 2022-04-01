22ebd01646bb13c1c2dc62479fb10a39b43aa95f
Abdi <letsbuildshhopify>
2022-04-01 02:12:05 +0100

    changes

--- app.js
+++ app.js
@@ -12,8 +12,15 @@ let contentH3 = document.getElementsByClassName("dynamicH3")[0];
 let contentP = document.getElementsByClassName("dynamicP")[0];
 let contentImg = document.getElementsByClassName("dynamicImg")
 let detailH2 = document.getElementsByClassName("dynamicH2")[0];
-let detailLi = document.querySelectorAll('.dynamicLi');
-let detailP = document.getElementsByClassName("dynamicP2")[0];
+let detailLi = document.querySelector('.dynamicLi1');
+let detailLi2 = document.querySelector('.dynamicLi2');
+let detailLi3 = document.querySelector('.dynamicLi3');
+let detailLi4 = document.querySelector('.dynamicLi4');
+let detailP = document.querySelector('.dynamicP2');
+let detailP2 = document.querySelector('.dynamicP3');
+let bedRooms = document.querySelector('.bedrooms');
+let squareFt = document.querySelector('.sqr-ft');
+let floor = document.querySelector('.floor');
 const propertyBtn = document.querySelectorAll('.anchor-button');
 const radiobtn = document.getElementById("r1");
 let aboutContainer = document.getElementsByClassName("about-content-info")[0];
@@ -32,39 +39,85 @@ let mobileProperties = document.querySelectorAll(".property");
 
 
 const one = {
-    propertyImg: ["galleryImg1.jpeg", "galleryImg2.jpeg", "galleryImg3.jpeg", "galleryImg2.jpeg"],
+    propertyImg: ["assets/spacejoy-tAuc4H7Qf9s-unsplash.jpg", "assets/steven-ungermann-Aac7IlKnYX8-unsplash.jpg", "assets/carl-gartman-eUvshcvtULI-unsplash.jpg", "assets/sidekix-media-WgkA3CSFrjc-unsplash.jpg"],
     h1: "105 BroadWay",
     h3: "London",
-    p: "1-Bed, 2-Bed from £485,950"
+    p: "1-Bed, 2-Bed from £485,950",
+    l1: "New-build development in London's Zone 3",
+    l2: "Virtually all properties to feature a private balcony ",
+    l3: "Designer kitchen plus appliances to each home",
+    l4: "Access to a Squash and Fitness Club",
+    p2: "Introducing Wimbledon Grounds: a 51,000 sq. m. mixed-use regeneration masterplan located on Plough Lane. The development comprises a brand new purpose-built stadium for AFC Wimbledon, encircled by a residential scheme with more than 600 homes, including shared ownership housing by Catalyst. The site is also home to all-new community, leisure and retail space, and is set to transform the town centre and surrounding area.",
+    p3: "RPJ Homes is proud to present Stadia One and Stadia Three — two superb new residential buildings at Wimbledon Grounds. Stadia One offers an impressive 364 new-build homes, while Stadia Three presents a sensational collection of 114 one, two and three bedroom apartments arranged across seven storeys. Residents can enjoy premier specifications as well as access to private external space in the form of either a balcony or roof terrace, whilst lifestyle facilities include an extensive hospitality suite within the adjacent stadium, retail facilities, plus a squash and fitness club. Two communal landscaped gardens feature at the development while a new public realm with a dedicated cycle route is also part of this exciting scheme.",
+    room: "2",
+    measurement: "545 sq ft (approx)",
+    floor: "Third Floor"
 };
 
 const two = {
-    propertyImg: ["hunter2.jpeg", "hunter3.jpeg", "hunter5.jpeg", "hunter7.jpeg"],
+    propertyImg: ["assets/sidekix-media-7jlVQPX8PLE-unsplash.jpg", "assets/r-architecture-QMo-jtdyAQU-unsplash.jpg", "assets/sidekix-media-VaGdhK-kI1c-unsplash.jpg", "assets/outsite-co-R-LK3sqLiBw-unsplash.jpg"],
     h1: "Pavilion",
     h3: "London",
-    p: "3-Bed House from 550,675"
+    p: "3-Bed House from 550,675",
+    l1: "TWO DOUBLE BEDROOMS",
+    l2: "LOTS OF STORAGE",
+    l3: "9 MINUTES WALK TO WEST DULWICH STATION",
+    l4: "GAS CENTRAL HEATING & DOUBLE GLAZED WINDOWS",
+    p2: "Located at 65-70 White Lion Street, a three-minute walk from both the popular Upper Street and Angel underground station, the cleared freehold development site spans 0.29 acres and has planning permission for a high quality new development arranged over six floors. Angel is one of most popular areas in the wider City Fringe market, with a wide array of amenities and excellent transport connections from Angel underground station, providing fast access to Kings Cross, the City and West End. A number of new mixed use developments have emerged across N1 in the past few years, adding to the retail offering and cementing Angel as a key submarket for TMT and other sectors.",
+    p3: "The new development will achieve a BREEAM rating of “Excellent” and will include high levels of natural daylight throughout with glazed frontages and internal light wells, with average floor to ceiling heights of three metres. There will be expansive roof terraces, two passenger lifts and 64 bicycle spaces, plus shower facilities at basement level.",
+    room: "4",
+    measurement: "680 sq ft (approx)",
+    floor: "2 Floors"
 };
 
 const three = {
-    propertyImg: ["sidekix-media-g51F6-WYzyU-unsplash.jpg", "francesca-tosolini-hCU4fimRW-c-unsplash.jpg", "sidekix-media-0sDzRgrN_pI-unsplash.jpg", "sidekix-media-oCw5_evbWyI-unsplash.jpg"],
+    propertyImg: ["assets/sidekix-media-g51F6-WYzyU-unsplash.jpg", "assets/francesca-tosolini-hCU4fimRW-c-unsplash.jpg", "assets/sidekix-media-0sDzRgrN_pI-unsplash.jpg", "assets/sidekix-media-oCw5_evbWyI-unsplash.jpg"],
     h1: "St Martin's Close",
     h3: "Birmingham",
-    p: "2-Bed from £235,000 "
+    p: "2-Bed from £235,000 ",
+    l1: "Heated Swimming Pool/Gym & Treatment Rooms",
+    l2: "Star Concierge & 24 Hour Security ",
+    l3: "Open Plan Living & Kitchen",
+    l4: "Landmark Views & Winter Garden",
+    p2: "This exceptional 676sqft 2-bedroom apartment sits on the 22nd floor of Eight Casson Square, Southbank Place and offers breath-taking views across the River Thames, Westminster, and the City. Flooded by light from the full-height windows, the living spaces have a light touch, with space to relax, entertain and watch the ever-changing panorama of the city. Eight Casson Square provides a selection of high quality designed 1-bed suites, 1 and 2-bed apartments and penthouses in a 30 storey Tower designed by Patel Taylor, with a new ticket hall and the new Waterloo underground station entrance at ground floor level. Finished to an exceptional level of specification, the apartments at 8 Casson Square provide sophisticated living within one of central London's most desirable locations.",
+    p3: "Located in one of London's most desirable and world-renowned riverside locations, Southbank Place lies on the banks of the River Thames adjacent to Westminster, Jubilee Gardens and The London Eye. Surrounded by an area steeped in history, heritage and culture, Southbank Place.",
+    room: "2",
+    measurement: "595 sq ft (approx)",
+    floor: "Sixth Floor"
 };
 
 const four = {
-    propertyImg: ["sidekix-media-t294_ZZP2pg-unsplash.jpg", "zac-gudakov-lk-APuu-T5A-unsplash.jpg", "spacejoy-8wDnylxemkk-unsplash.jpg", "steven-ungermann-1AF5hP6F4tI-unsplash.jpg"],
+    propertyImg: ["assets/sidekix-media-t294_ZZP2pg-unsplash.jpg", "assets/zac-gudakov-lk-APuu-T5A-unsplash.jpg", "assets/spacejoy-8wDnylxemkk-unsplash.jpg", "assets/steven-ungermann-1AF5hP6F4tI-unsplash.jpg"],
     h1: "The Grand Exchange",
     h3: "London",
-    p: "1-Bed Apartment, 2-Bed Apartment "
+    p: "4 Bedrooms 2 Bathrooms ",
+    l1: "Spacious 4 Bedrooms & 2 Bathrooms",
+    l2: "Mid Terraced with Driveway",
+    l3: "Designer kitchen plus appliances to each home",
+    l4: "Double Glazed Window & Central Heating",
+    p2: "Spacious Four bedrooms property available in the well-known Wembley area; extremely close to bus stations, local shops and within easy access to London Outlet and Wembley Stadium. On the ground floor you will find two large reception rooms including a dining room off the kitchen, a shower room and an outbuilding at the rear of the garden. The first floor consists of four bedrooms and a family bathroom.",
+    p3: "Rare opportunity to acquire this elegant and contemporary home. Boasting a charming courtyard garden and a stunning rooftop terrace with direct views of Nine Elms skyline. This lovely home has an abundance of light and space throughout, two bathrooms, large kitchen dining area and expertly located a short stroll from the new Nine Elms station, Early viewing's highly recommended.",
+    room: "4",
+    measurement: "620 sq ft (approx)",
+    floor: "Three Floors"
 };
 
 const five = {
-    propertyImg: ["sidekix-media-1vMz2_MclrM-unsplash.jpg", "ralph-ravi-kayden-JDBVXignFdA-unsplash.jpg", "sidekix-media-8qNuR1lIv_k-unsplash.jpg", "francesca-tosolini-FX1EbT-jKBQ-unsplash.jpg"],
+    propertyImg: ["assets/point3d-commercial-imaging-ltd-xON7AlJZemw-unsplash.jpg", "assets/zac-gudakov-RMnc22CwVVA-unsplash.jpg", "assets/rune-enstad-UXFJ-6Zj27M-unsplash.jpg", "assets/michael-oxendine-NIC-IhNpsNs-unsplash.jpg"],
     h1: "Queen's Court",
     h3: "London",
-    p: "1-Bed Apartment, 2-Bed Apartment "
+    p: "1-Bed Apartment, 2-Bed Apartment ",
+    l1: "Grand master bedroom & New bathroom",
+    l2: "Imposing reception room with Large kitchen/diner",
+    l3: "Long leasehold with local amenities",
+    l4: "Secluded private garden",
+    p2: "A Belgravia apartment conveniently located in a vibrant location on the door step of Elizabeth street and Ecclestone Yard.Ebury Street is located close to Elizabeth Street and Ecclestone Yard in the heart of Belgravia. Victoria underground and mainline station is 0.3 miles away, and Sloane Square underground station is 0.5 miles away (all times and distances are approximate).",
+    p3: "The apartment comprises on the second floor a large living room with working gas fireplace overlooking Ebury Street and a separate kitchen at the rear. There is a guest bathroom with shower on the first-floor half landing. The second floor has a large charming principal bedroom with beamed ceilings opening onto a windowed dressing room with three large walk in cupboards and a bathroom. The second smaller double bedroom doubles as an office while the bathroom can service both bedrooms. This floor could easily be converted into an extra bathroom and small bedroom. There is also an unconverted attic reached via the landing which runs the length of the upper floor. The three flat owners of the building have the right to manage.",
+    room: "2",
+    measurement: "500 sq ft (approx)",
+    floor: "Third Floor"
 };
+
 
 const aboutOne = {
     h2: "WHO WE ARE",
@@ -77,7 +130,7 @@ const aboutTwo = {
     h2: "WHAT WE DO",
     p: "We specialise in residential-led mixed-use developments and have delivered successful projects across London, from Kensington to Tower Hamlets and from Barnet to Shoreditch. Bespoke design and exceptional quality characterise our developments which are built to unlock value, enhance the local environment and respect local communities.",
     p2: "Regal London is a fully integrated business operating across the entire lifecycle of the asset, which means that we put the customer at the centre of everything we do, ensuring the delivery of outstanding quality and service at every stage of the pre and post development process. This includes land assembly, land use & planning strategy, stakeholder engagement, sales & marketing, construction, customer care and asset management.",
-    p3: "Our integrated approach means design and delivery is fundamental through the development lifecycle: we believe in creating some of London’s most aesthetic places in which to live, work and play."
+    p3: "Our integrated approach means design and delivery is fundamental through the development lifecycle: we believe in creating some of London's most aesthetic places in which to live, work and play."
 };
 
 const aboutThree = {
@@ -114,6 +167,15 @@ propertyBtn.forEach((btn) => btn.addEventListener('click', function () {
         contentH1.innerHTML = one.h1;
         contentH3.innerHTML = one.h3;
         contentP.innerHTML = one.p;
+        detailLi.innerHTML = one.l1;
+        detailLi2.innerHTML = one.l2;
+        detailLi3.innerHTML = one.l3;
+        detailLi4.innerHTML = one.l4;
+        detailP.innerHTML = one.p2;
+        detailP2.innerHTML = one.p3;
+        bedRooms.innerHTML = one.room;
+        squareFt.innerHTML = one.measurement;
+        floor.innerHTML = one.floor;
         for (let i = 0; i < contentImg.length; i++) {
             contentImg[i].src = one.propertyImg[i];
         }
@@ -121,6 +183,15 @@ propertyBtn.forEach((btn) => btn.addEventListener('click', function () {
         contentH1.innerHTML = two.h1;
         contentH3.innerHTML = two.h3;
         contentP.innerHTML = two.p;
+        detailLi.innerHTML = two.l1;
+        detailLi2.innerHTML = two.l2;
+        detailLi3.innerHTML = two.l3;
+        detailLi4.innerHTML = two.l4;
+        detailP.innerHTML = two.p2;
+        detailP2.innerHTML = two.p3;
+        bedRooms.innerHTML = two.room;
+        squareFt.innerHTML = two.measurement;
+        floor.innerHTML = two.floor;
         for (let i = 0; i < contentImg.length; i++) {
             contentImg[i].src = two.propertyImg[i];
         }
@@ -128,6 +199,15 @@ propertyBtn.forEach((btn) => btn.addEventListener('click', function () {
         contentH1.innerHTML = three.h1;
         contentH3.innerHTML = three.h3;
         contentP.innerHTML = three.p;
+        detailLi.innerHTML = three.l1;
+        detailLi2.innerHTML = three.l2;
+        detailLi3.innerHTML = three.l3;
+        detailLi4.innerHTML = three.l4;
+        detailP.innerHTML = three.p2;
+        detailP2.innerHTML = three.p3;
+        bedRooms.innerHTML = three.room;
+        squareFt.innerHTML = three.measurement;
+        floor.innerHTML = three.floor;
         for (let i = 0; i < contentImg.length; i++) {
             contentImg[i].src = three.propertyImg[i];
         }
@@ -136,6 +216,15 @@ propertyBtn.forEach((btn) => btn.addEventListener('click', function () {
         contentH1.innerHTML = four.h1;
         contentH3.innerHTML = four.h3;
         contentP.innerHTML = four.p;
+        detailLi.innerHTML = four.l1;
+        detailLi2.innerHTML = four.l2;
+        detailLi3.innerHTML = four.l3;
+        detailLi4.innerHTML = four.l4;
+        detailP.innerHTML = four.p2;
+        detailP2.innerHTML = four.p3;
+        bedRooms.innerHTML = four.room;
+        squareFt.innerHTML = four.measurement;
+        floor.innerHTML = four.floor;
         for (let i = 0; i < contentImg.length; i++) {
             contentImg[i].src = four.propertyImg[i];
         }
@@ -144,6 +233,15 @@ propertyBtn.forEach((btn) => btn.addEventListener('click', function () {
         contentH1.innerHTML = five.h1;
         contentH3.innerHTML = five.h3;
         contentP.innerHTML = five.p;
+        detailLi.innerHTML = five.l1;
+        detailLi2.innerHTML = five.l2;
+        detailLi3.innerHTML = five.l3;
+        detailLi4.innerHTML = five.l4;
+        detailP.innerHTML = five.p2;
+        detailP2.innerHTML = five.p3;
+        bedRooms.innerHTML = five.room;
+        squareFt.innerHTML = five.measurement;
+        floor.innerHTML = five.floor;
         for (let i = 0; i < contentImg.length; i++) {
             contentImg[i].src = five.propertyImg[i];
         }
